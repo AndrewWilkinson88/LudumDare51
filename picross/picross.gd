d9a4045a398f00ed7a3b184ac2b922d4653a6a18
Kevin Kohler <kvn.khlr@gmail.com>
2022-10-01 12:20:29 -0400

    Number UI added

--- picross/picross.gd
+++ picross/picross.gd
@@ -3,6 +3,9 @@ extends Node2D
 class_name PicrossPuzzle
 
 var THRESHOLD : float = .1
+var UI_OFFSET : int = 120
+
+var gamefield : Node2D
 
 # Texture for the input puzzle
 var curPuzzleTexture : Texture
@@ -10,6 +13,8 @@ var curPuzzleTexture : Texture
 var cellTexture : Texture
 var yesTexture : Texture
 var noTexture : Texture
+
+var font : DynamicFont
 
 #Holds the Y and X picross arrays.  IE if the image were 0011001100 the array would hold (2,2)
 var curPuzzlePicrossY
@@ -25,11 +30,16 @@ var puzzleHeight :int
 func _ready():
 	self.scale.x = 2
 	self.scale.y = 2
+	font = DynamicFont.new()	
+	font.font_data = load("res://fonts/rainyhearts.ttf")
+	font.size = 32
+	font.extra_spacing_bottom = -3
+	font.extra_spacing_top = -3
 	cellTexture  = load("res://picross/cell.png")
 	yesTexture  = load("res://picross/picrosspositive.png")
 	noTexture  = load("res://picross/x.png")
 	
-	createPicrossPuzzle("res://picross/face2.png")
+	createPicrossPuzzle("res://picross/smiley.png")
 	pass # Replace with function body.
 	
 #Load and setup a picross puzzle by name
@@ -44,7 +54,6 @@ func createPicrossPuzzle(imageName):
 	puzzleWidth = image.get_width()
 	puzzleHeight = image.get_height()
 	
-	var textToPrint = ""
 	var c : Color
 	var chainCounter :int  = 0
 	var firstPerLine :bool
@@ -68,18 +77,9 @@ func createPicrossPuzzle(imageName):
 			# or at the end of line then we're ready to create a full entry for this picross value
 			if chainCounter > 0 && (c.a < THRESHOLD || y == puzzleHeight-1) :
 				curPuzzlePicrossY[x].append( chainCounter)
-				if !firstPerLine :
-					textToPrint += " , "
-				textToPrint += str(chainCounter) 
 				firstPerLine = false
 				chainCounter = 0
-		if firstPerLine :
-			textToPrint += '-'
-		textToPrint += "\n"
 	
-	print(textToPrint)
-	print()
-	textToPrint = ""
 	chainCounter = 0
 	
 	for y in puzzleHeight:
@@ -91,23 +91,20 @@ func createPicrossPuzzle(imageName):
 				chainCounter +=1
 			if chainCounter > 0 && (c.a < THRESHOLD || x == puzzleWidth-1) :
 				curPuzzlePicrossX[y].append( chainCounter)
-				if !firstPerLine :
-					textToPrint += " , "
-				textToPrint += str(chainCounter) 
 				firstPerLine = false
 				chainCounter = 0
-		if firstPerLine :
-			textToPrint += '-'
-		textToPrint += "\n"
-		
-	print(textToPrint)
+			
 	image.unlock()
-	
-	#add_child(sprite)
+		
 	_generateGamefield()
+	_generatePicrossText()
 	pass
 
 func _generateGamefield():
+	gamefield = Node2D.new()
+	gamefield.position.x = UI_OFFSET
+	gamefield.position.y = UI_OFFSET
+	add_child(gamefield)
 	picrossField = []
 	for x in puzzleWidth:
 		picrossField.append([])
@@ -117,25 +114,46 @@ func _generateGamefield():
 			cell.setup(cellTexture, yesTexture, noTexture)
 			cell.position.x = x * cellTexture.get_width()
 			cell.position.y = y * cellTexture.get_height()
-			add_child(cell)
-			
-#			if (x + y) % 3 == 0:
-#				cell.setOverlay(PicrossCell.CELL_STATE.YES)
-#			elif (x + y+1) % 3:
-#				cell.setOverlay(PicrossCell.CELL_STATE.NO)
+			gamefield.add_child(cell)
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _generatePicrossText():
 	
+	for x in puzzleWidth:
+		var label = Label.new()
+		label.set("custom_fonts/font", font);
+		label.set_size(Vector2(cellTexture.get_width(), UI_OFFSET-3))
+		label.align = Label.ALIGN_CENTER
+		label.valign = Label.VALIGN_BOTTOM
+		add_child(label)
+		label.set_position(Vector2(UI_OFFSET + x * cellTexture.get_width(), 0))	
+		for y in curPuzzlePicrossY[x].size():
+			label.text += str(curPuzzlePicrossY[x][y]) 
+			if(y != curPuzzlePicrossY[x].size()-1):
+				label.text += "\n"
+	
+	for y in puzzleHeight:
+		var label = Label.new()
+		label.set("custom_fonts/font", font);
+		label.set_size(Vector2(UI_OFFSET-3, cellTexture.get_height()))
+		label.align = Label.ALIGN_RIGHT
+		label.valign = Label.VALIGN_CENTER
+		add_child(label)
+		label.set_position( Vector2(0,UI_OFFSET + y * cellTexture.get_height()))
+		for x in curPuzzlePicrossX[y].size():
+			label.text += str(curPuzzlePicrossX[y][x])
+			if(x != curPuzzlePicrossX[y].size()-1):
+				label.text +=  "  "
+	
+	pass
+
 var leftButtonDown : bool = false
 var dragType = PicrossCell.CELL_STATE.UNDEFINED
 var rightButtonDown : bool = false
 
 func _input(event):
 	if event is InputEventMouse:
-		var pos = to_local(event.position)
+		var pos = gamefield.to_local(event.position)
 		var x :int = pos.x / cellTexture.get_width()
 		var y :int = pos.y / cellTexture.get_height()
 		var isValidCell = x >= 0 and x < puzzleWidth and y >= 0 and y < puzzleHeight
@@ -151,10 +169,10 @@ func _input(event):
 				rightButtonDown = true
 			elif(event.button_index == BUTTON_RIGHT and not event.pressed): 
 				rightButtonDown = false
-
-		print ("x : " + str(x) + "   y : " + str(y))
+		
 		if leftButtonDown and isValidCell:
 			picrossField[x][y].setState(dragType)
 		if rightButtonDown and isValidCell:
 			picrossField[x][y].setState(PicrossCell.CELL_STATE.NO)
 	pass
+
